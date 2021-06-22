f32d14f48b834beaa00e7d4903b05432085edb19
Bartosz Krakowiak <bartosz_krakowiak@protonmail.com>
2021-06-23 01:03:07 +0200

    add example backend code

---
+++ backend/app/crud.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Session
+
+from . import models
+
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
-from typing import Optional
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -10,6 +25,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
